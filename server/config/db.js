4847754dcfdacf28c3f42de5333463039b57cf74
Rahulgit3D2Y <rc2504000@gmail.com>
2024-07-10 18:43:07 +0530

    3

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
-
-const connectDb = async () => {
+const mongoose = require('mongoose');
+const connectDB = async () => {
+  
   try {
+    mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database connected : ${conn.connection.host}`);
+    console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
   }
-};
 
-module.exports = connectDb;
+}
+
+module.exports = connectDB;
